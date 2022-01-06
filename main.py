f77a64f24bcf8d1c3bda0b1d80fb39a513e094a4
chebroluven <80992272+venabhi@users.noreply.github.com>
2022-01-06 20:21:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "abhi"
+print(name)
+
+
+name = input("what is your name? ")
+length = len(name)
+print(length)
+
+print("welcome to the band")
+city = input("which city were you grew up?\n")
+print(city)
+pet = input("what is your pet name?\n")
+print(pet)
+print("my band name is " + city + " " +pet)
+
+
